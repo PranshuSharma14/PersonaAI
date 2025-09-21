c337401896e46d7b76a28ef0e40b7d0c3ffa949d
PranshuSharma14 <pranshusharmaindia@gmail.com>
2025-09-21 17:02:47 +0530

    litle change

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -188,7 +188,7 @@ export const SignUpView = ()=>{
                                 </Alert>
                             )}
                             <Button type="submit" className="w-full" disabled={pending}>
-                                Sign In 
+                                Sign Up     
                             </Button>
                             <div className="after: border-border relative text-center text-sm after:absolute after:insert-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                                 <span className="bg-card text-muted-foreground relative z-10 px-2">Or continue with</span>
