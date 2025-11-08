7771ecdb2b492003c72436378b60a4f636baded8
PranshuSharma14 <pranshusharmaindia@gmail.com>
2025-11-09 00:48:08 +0530

    meeting-variants

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -22,13 +22,26 @@ export const Meetingsview =() =>{
 
     return (
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <DataTable data={data.items} columns={columns} onRowClick={(row) => router.push(`/meetings/${row.id}`)}/>
+            {data.items.length === 0 ? (
+                <EmptyState 
+                    title="Create your first meeting"
+                    description="Schedule a meeting to connect with others. Each meeting lets you collaborate, share ideas, and interact with participants in real time."
+                />
+            ) : (
+            <>
+            <DataTable 
+                data={data.items} 
+                columns={columns} 
+                onRowClick={(row) => router.push(`/meetings/${row.id}`)}
+            />
             <DataPagination
                 page={filters.page}
                 totalPages={data.totalPages}
-                onPageChange={(page) => setFilters({page}) }
+                onPageChange={(page) => setFilters({ page })}
             />
-             <EmptyState title="Create your first meeting" description="Schedule a meeting to connect with others. Each meeting lets you collaborate,share ideas, and interact with participants in real time"/>
+            </>
+            )}
+
         </div>
     )
 }
